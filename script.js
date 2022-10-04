4b44ee8cd094d4228af028e17084f54140a5e7b6
spacechimp <syimismail@gmail.com>
2022-10-04 17:11:09 +0800

    added more texts YESSZAAAAA

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-var i=0,text;
+var i=0, text, j=0, text3;
 text = "Hello! I know you got questions for me. So, go ahead my child. Ask me anything..."
+text3 = "You! What have you done??!!"
 
 function firstText(){
+    document.getElementById('orb-name').style.display = "block";
     if(i<text.length){
         document.getElementById("text").innerHTML += text.charAt(i);
         i++;
-        setTimeout(firstText, 80);
+        setTimeout(firstText,90);
+    }
+    if(i === text.length){
+        document.getElementById('arrow-next').style.display = "block";
+
+    }
+}
+
+function goNext(){
+    document.getElementById('first').style.display = "hidden";
+    document.getElementById('second').style.display = "block";
+}
+
+function selectOption(){
+    document.getElementById('second').style.display = "hidden";
+    thirdText()
+}
+
+
+function thirdText(){
+    document.getElementById('third').style.display = "block";
+    if(j<text3.length){
+        document.getElementById("text3").innerHTML += text3.charAt(j);
+        j++;
+        setTimeout(thirdText,70);
     }
 }
